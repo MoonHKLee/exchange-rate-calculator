843a558407d273cdfe89a89b18566ae84247fe03
MoonHKLee <mf311110@gmail.com>
2023-05-11 23:31:41 +0900

    Initial commit

---
+++ src/test/java/lee/moonhyuk/exchangeratecalculator/ExchangeRateCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.moonhyuk.exchangeratecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
