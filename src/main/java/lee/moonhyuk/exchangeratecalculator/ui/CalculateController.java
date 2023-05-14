40c7034a26d76511dc5e4ba4f84fc824747b2b87
MoonHKLee <mf311110@gmail.com>
2023-05-14 14:53:16 +0900

    feat(controller) : 화면 구현.

--- src/main/java/lee/moonhyuk/exchangeratecalculator/ui/CalculateController.java
+++ src/main/java/lee/moonhyuk/exchangeratecalculator/ui/CalculateController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,10 +14,16 @@ public class CalculateController {
 
     private final ExchangeRateServiceImpl exchangeRateService;
     @GetMapping("/")
-    public String getIndexPage(Model model, @RequestParam("exchangeRateKey") String exchangeRateKey) {
+    public String getIndexPage(Model model, @RequestParam(value = "exchangeRateKey", defaultValue = "USDKRW") String exchangeRateKey) {
         String result = exchangeRateService.getExchangeRate(exchangeRateKey);
-        model.addAttribute("USDKRW", result);
+        model.addAttribute("rate", result + " KRW/USD");
         return "index";
+    }
+
+    @GetMapping("/exchangeRate")
+    @ResponseBody
+    public String getExchangeRate(@RequestParam(value = "exchangeRateKey", defaultValue = "USDKRW") String exchangeRateKey) {
+        return exchangeRateService.getExchangeRate(exchangeRateKey);
     }
 
 }
