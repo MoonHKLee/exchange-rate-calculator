8cd2a788a9b2ce6cf96d202ddd38e264c0e4ae3b
MoonHKLee <mf311110@gmail.com>
2023-05-14 12:54:21 +0900

    feat(exchangeRateFactory) : USD 바탕으로 원하는 KEY값의 환율을 구하도록 구현.

---
+++ src/main/java/lee/moonhyuk/exchangeratecalculator/RealTimeExchangeRateFactory.java
@@ -0,0 +1,28 @@
+package lee.moonhyuk.exchangeratecalculator;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class RealTimeExchangeRateFactory {
+    private final String json;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    public RealTimeExchangeRateFactory(String json) {
+        this.json = json;
+    }
+
+    public String getExchangeRate(String key) {
+        try {
+            JsonNode jsonNode = objectMapper.readTree(json);
+            double exchangeRate = jsonNode.get("quotes").get(key).asDouble();
+            DecimalFormat decimalFormat = new DecimalFormat("#,###.00");
+            decimalFormat.setRoundingMode(RoundingMode.DOWN);
+            return decimalFormat.format(exchangeRate);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("존재하지 않는 값입니다.");
+        }
+    }
+}
